36d14242b68291f0ca77647e94ee1fb911e9d2a3
VanshJ33 <vansh@arobuz.com>
2026-03-09 18:47:21 +0530

    fixed things

--- backend/dist/common/decorators/roles.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-export type Role = 'customer' | 'admin' | 'superadmin';
-export declare const ROLES_KEY = "roles";
-export declare const Roles: (...roles: Role[]) => import("@nestjs/common").CustomDecorator<string>;
--- backend/dist/modules/admin/admin.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { Model } from 'mongoose';
-import { AdminUserDocument } from './schemas/admin-user.schema';
-export declare class AdminModule implements OnModuleInit {
-    private adminModel;
-    constructor(adminModel: Model<AdminUserDocument>);
-    onModuleInit(): Promise<void>;
-}
